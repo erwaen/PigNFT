eeaafee45f5af3a49632cc202b128d8523da3f08
erwaen <erikwasmosy98@hotmail.com>
2022-02-27 16:38:00 -0300

    Se cargo los datos de las imagenes boca, fondos, nariz, ojos, oreja, ropa

---
+++ data.py
@@ -0,0 +1,267 @@
+####### RANKS #######
+ranks = [
+            "Bronze",
+            "Silver",
+            "Gold",
+            "Platinum",
+            "Diamond"
+        ]
+
+
+images = {
+    "Mouth": [
+                {
+                    "name": "Normal",
+                    "file_name": "boca1",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Tongue",
+                    "file_name": "boca2",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Face mask",
+                    "file_name": "boca3",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Smoking",
+                    "file_name": "boca4",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "2 teeth",
+                    "file_name": "boca5",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Teeth",
+                    "file_name": "boca6",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Sharp teeth",
+                    "file_name": "boca7",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Normal 2",
+                    "file_name": "boca8",
+                    "rank": ranks[0],
+                }
+    ],
+    "Background": [
+                    {
+                        "name": "Light Blue",
+                        "file_name": "color1",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Yellow",
+                        "file_name": "color2",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Green",
+                        "file_name": "color3",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Pink",
+                        "file_name": "color4",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Red",
+                        "file_name": "color5",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Blue",
+                        "file_name": "color6",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Lemon Green",
+                        "file_name": "color7",
+                        "rank": ranks[0],
+                    },
+    ],
+    "Nose": [
+                {
+                    "name": "Nose 1",
+                    "file_name": "nariz1",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Nose 2",
+                    "file_name": "nariz2",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Nose 3",
+                    "file_name": "nariz3",
+                    "rank": ranks[0],
+                },
+    ],
+    "Eyes": [
+                {
+                    "name": "Dot",
+                    "file_name": "ojos1",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Tired",
+                    "file_name": "ojos2",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Avocado",
+                    "file_name": "ojos4",
+  
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Lemon",
+                    "file_name": "ojos5",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Angry",
+                    "file_name": "ojos6",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Cry",
+                    "file_name": "ojos7",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Made up",
+                    "file_name": "ojos8",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Bored",
+                    "file_name": "ojos9",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Curious",
+                    "file_name": "ojos10",
+                    "rank": ranks[0],
+                },
+    ],
+    "Ears": [
+                {
+                    "name": "Normal",
+                    "file_name": "oreja1",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Elf",
+                    "file_name": "oreja2",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Rabbit",
+                    "file_name": "oreja3",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Elephant",
+                    "file_name": "oreja4",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Bat",
+                    "file_name": "oreja5",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Koala",
+                    "file_name": "oreja6",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Piggie",
+                    "file_name": "oreja7",
+                    "rank": ranks[0],
+                },
+                {
+                    "name": "Zombie",
+                    "file_name": "oreja8",
+                    "rank": ranks[0],
+                },
+    ],
+    "Clothing": [
+                    {
+                        "name": "Boka rj",
+                        "file_name": "ropa1",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Pijama",
+                        "file_name": "ropa2",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Suit",
+                        "file_name": "ropa3",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Tie",
+                        "file_name": "ropa4",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Soldier",
+                        "file_name": "ropa5",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Farmer",
+                        "file_name": "ropa6",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "FC Rekutu",
+                        "file_name": "ropa7",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Checa",
+                        "file_name": "ropa8",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Shirt",
+                        "file_name": "ropa9",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Braziul",
+                        "file_name": "ropa10",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Cerro",
+                        "file_name": "ropa11",
+                        "rank": ranks[0],
+                    },
+                    {
+                        "name": "Olimpia",
+                        "file_name": "ropa12",
+                        "rank": ranks[0],
+                    }           
+    ],
+    "Shape": [
+                {
+                    "name": "Normal",
+                    "file_name": "silueta1",
+                    "rank": ranks[0],
+                },
+    ]
+}
