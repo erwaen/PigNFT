f99f7a1e3ba828fbd9b63e750f7bb76f396f9cae
erwaen <erikwasmosy98@hotmail.com>
2022-02-27 17:06:15 -0300

    Fix en la data y creacion de main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import random
 import data as my_data
 
+def create_new_image():
+    new_image = {}
+
+    for key in my_data.images:
+        probability_key_trait = []
+        for item in my_data.images[key]:
+            print(item, "\n")
+        # new_image[key] = random.choice()
+
 def main():
-    print("main")
+    create_new_image()
 
 main()
